e353807af8d0a0cec136e6b328581fc0554637c9
zouy3 <360033617@qq.com>
2017-07-05 20:28:09 +0800

    7.5office

---
+++ mysql.py
@@ -0,0 +1,31 @@
+import pymysql
+
+
+def connect():
+	conn=pymysql.connect(
+	host='localhost',
+	port=3306,
+	user='root',
+	passwd='lrm12350039',
+	db='haocai',
+    charset='utf8'
+	)
+	cur=conn.cursor()
+	return conn, cur;
+
+def query(sql):
+	conn,cur=connect()
+	cur.execute(sql)
+	re = cur.fetchall();
+	cur.close()
+	return re
+
+def change(sql):
+	conn,cur=connect()
+	cur.execute(sql)
+	connect.commit()
+	cursor.close()
+	return
+	
+output = query("select * from goodsnum")
+print (output)
